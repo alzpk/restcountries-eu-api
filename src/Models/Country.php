798f1a17b982ec2771e8e44066009c0d303002bb
Michael Hansen <michael.hansen@quickorder.io>
2021-09-15 20:23:56 +0200

    Added models

---
+++ src/Models/Country.php
@@ -0,0 +1,353 @@
+<?php
+
+namespace Alzpk\RestCountriesEuAPI\Models;
+
+use Alzpk\RestCountriesEuAPI\ValueObjects\AlternativeSpelling;
+use Alzpk\RestCountriesEuAPI\ValueObjects\Border;
+use Alzpk\RestCountriesEuAPI\ValueObjects\CallingCode;
+use Alzpk\RestCountriesEuAPI\ValueObjects\Currency;
+use Alzpk\RestCountriesEuAPI\ValueObjects\Language;
+use Alzpk\RestCountriesEuAPI\ValueObjects\RegionalBloc;
+use Alzpk\RestCountriesEuAPI\ValueObjects\Timezone;
+use Alzpk\RestCountriesEuAPI\ValueObjects\TopLevelDomain;
+use Alzpk\RestCountriesEuAPI\ValueObjects\Translation;
+
+class Country
+{
+    private string $name;
+    private string $nativeName;
+    private int $numericCode;
+    private string $alpha2Code;
+    private string $alpha3Code;
+    private string $region;
+    private string $subregion;
+    private int $population;
+    private ?float $lat;
+    private ?float $lng;
+    private string $demonym;
+    private float $area;
+    private string $flag;
+    private ?string $cioc;
+
+    /** @var list<TopLevelDomain> $topLevelDomain */
+    private array $topLevelDomain;
+    /** @var list<CallingCode> $callingCodes */
+    private array $callingCodes;
+    /** @var list<AlternativeSpelling> $altSpellings */
+    private array $altSpellings;
+    /** @var list<Timezone> $timezones */
+    private array $timezones;
+    /** @var list<Border> $borders */
+    private ?array $borders;
+    /** @var list<Currency> $currencies */
+    private array $currencies;
+    /** @var list<Language> $languages */
+    private array $languages;
+    /** @var list<Translation> $translations */
+    private array $translations;
+    /** @var list<RegionalBloc> $regionalBlocs */
+    private array $regionalBlocs;
+
+    /**
+     * @param list<TopLevelDomain> $topLevelDomain
+     * @param list<CallingCode> $callingCodes
+     * @param list<AlternativeSpelling> $altSpellings
+     * @param list<Timezone> $timezones
+     * @param list<Border>|null $borders
+     * @param list<Currency> $currencies
+     * @param list<Language> $languages
+     * @param list<Translation> $translations
+     * @param list<RegionalBloc> $regionalBlocs
+     */
+    public function __construct(string $name, string $nativeName, int $numericCode, string $alpha2Code,
+                                string $alpha3Code, string $region, string $subregion, int $population,
+                                ?float $lat, ?float $lng, string $demonym, float $area, string $flag,
+                                ?string $cioc, array $topLevelDomain, array $callingCodes, array $altSpellings,
+                                array $timezones, ?array $borders, array $currencies, array $languages,
+                                array $translations, array $regionalBlocs)
+    {
+        $this->name = $name;
+        $this->nativeName = $nativeName;
+        $this->numericCode = $numericCode;
+        $this->alpha2Code = $alpha2Code;
+        $this->alpha3Code = $alpha3Code;
+        $this->region = $region;
+        $this->subregion = $subregion;
+        $this->population = $population;
+        $this->lat = $lat;
+        $this->lng = $lng;
+        $this->demonym = $demonym;
+        $this->area = $area;
+        $this->flag = $flag;
+        $this->cioc = $cioc;
+        $this->topLevelDomain = $topLevelDomain;
+        $this->callingCodes = $callingCodes;
+        $this->altSpellings = $altSpellings;
+        $this->timezones = $timezones;
+        $this->borders = $borders;
+        $this->currencies = $currencies;
+        $this->languages = $languages;
+        $this->translations = $translations;
+        $this->regionalBlocs = $regionalBlocs;
+    }
+
+    public static function fromResponse(array $response): self
+    {
+        return new self(
+            $response['name'],
+            $response['nativeName'],
+            intval($response['numericCode']),
+            $response['alpha2Code'],
+            $response['alpha3Code'],
+            $response['region'],
+            $response['subregion'],
+            intval($response['population']),
+            isset($response['latlng'][0]) ? floatval($response['latlng'][0]) : null,
+            isset($response['latlng'][1]) ? floatval($response['latlng'][1]) : null,
+            $response['demonym'],
+            floatval($response['area']),
+            $response['flag'],
+            $response['cioc'],
+            array_map(function(string $value) {
+                return TopLevelDomain::fromString($value);
+            }, $response['topLevelDomain']),
+            array_map(function(string $value) {
+                return CallingCode::fromString($value);
+            }, $response['callingCodes']),
+            array_map(function(string $value) {
+                return AlternativeSpelling::fromString($value);
+            }, $response['altSpellings']),
+            array_map(function(string $value) {
+                return Timezone::fromString($value);
+            }, $response['timezones']),
+            array_map(function(string $value) {
+                return Border::fromString($value);
+            }, $response['borders']),
+            array_map(function(array $response) {
+                return Currency::fromResponse($response);
+            }, $response['currencies']),
+            array_map(function(array $response) {
+                return Language::fromResponse($response);
+            }, $response['languages']),
+            array_map(function(?string $value, ?string $key) {
+                return Translation::fromKeyAndValue($key ?? '', $value ?? '');
+            }, $response['translations'], array_keys($response['translations'])),
+            array_map(function(array $response) {
+                return RegionalBloc::fromResponse($response);
+            }, $response['regionalBlocs'])
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNativeName(): string
+    {
+        return $this->nativeName;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumericCode(): int
+    {
+        return $this->numericCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlpha2Code(): string
+    {
+        return $this->alpha2Code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlpha3Code(): string
+    {
+        return $this->alpha3Code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRegion(): string
+    {
+        return $this->region;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubregion(): string
+    {
+        return $this->subregion;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPopulation(): int
+    {
+        return $this->population;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getLat(): ?float
+    {
+        return $this->lat;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getLng(): ?float
+    {
+        return $this->lng;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDemonym(): string
+    {
+        return $this->demonym;
+    }
+
+    /**
+     * @return float
+     */
+    public function getArea(): float
+    {
+        return $this->area;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFlag(): string
+    {
+        return $this->flag;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCioc(): ?string
+    {
+        return $this->cioc;
+    }
+
+    /**
+     * @return list<TopLevelDomain>
+     */
+    public function getTopLevelDomain(): array
+    {
+        return $this->topLevelDomain;
+    }
+
+    /**
+     * @return list<CallingCode>
+     */
+    public function getCallingCodes(): array
+    {
+        return $this->callingCodes;
+    }
+
+    /**
+     * @return list<AlternativeSpelling>
+     */
+    public function getAltSpellings(): array
+    {
+        return $this->altSpellings;
+    }
+
+    /**
+     * @return list<Timezone>
+     */
+    public function getTimezones(): array
+    {
+        return $this->timezones;
+    }
+
+    /**
+     * @return list<Border>|null
+     */
+    public function getBorders(): ?array
+    {
+        return $this->borders;
+    }
+
+    /**
+     * @return list<Currency>
+     */
+    public function getCurrencies(): array
+    {
+        return $this->currencies;
+    }
+
+    /**
+     * @return list<Language>
+     */
+    public function getLanguages(): array
+    {
+        return $this->languages;
+    }
+
+    /**
+     * @return list<Translation>
+     */
+    public function getTranslations(): array
+    {
+        return $this->translations;
+    }
+
+    /**
+     * @return list<RegionalBloc>
+     */
+    public function getRegionalBlocs(): array
+    {
+        return $this->regionalBlocs;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'native_name' => $this->nativeName,
+            'numeric_code' => $this->numericCode,
+            'alpha_2_code' => $this->alpha2Code,
+            'alpha_3_code' => $this->alpha3Code,
+            'region' => $this->region,
+            'subregion' => $this->subregion,
+            'population' => $this->population,
+            'lat' => $this->lat,
+            'lng' => $this->lng,
+            'demonym' => $this->demonym,
+            'area' => $this->area,
+            'flag' => $this->flag,
+            'cioc' => $this->cioc,
+            'top_level_domain' => $this->topLevelDomain,
+            'calling_codes' => $this->callingCodes,
+            'alt_spellings' => $this->altSpellings,
+            'timezones' => $this->timezones,
+            'borders' => $this->borders,
+            'currencies' => $this->currencies,
+            'languages' => $this->languages,
+            'translations' => $this->translations,
+            'regional_blocs' => $this->regionalBlocs,
+        ];
+    }
+}
